41e6231f5b936adba6a7b4950a6b7efecc170a6b
Unweptash <58473034+Unweptash@users.noreply.github.com>
2022-05-19 23:36:24 +0200

    Update functions.py
    fixed dictionary mention

--- functions.py
+++ functions.py
@@ -2,149 +2,149 @@ from dictionaries import *
 import math as math
 
 
-def powerup_resets():
+def powerup_resets(powerup1, powerup2, paddles):
     #reset powerup1        
     if 8000 < millis() - main["other"]["last_hit1"] < 9000 and main["other"]["counter1"] == 1:
         main["other"]["last_hit1"] = 0
-        powerup1_reset()
+        powerup1_reset(powerup1, paddles)
 
     #reset powerup2        
     if 8000 < millis() - main["other"]["last_hit2"] < 9000 and main["other"]["counter2"] == 1:
         main["other"]["last_hit2"] = 0
-        powerup2_reset()
+        powerup2_reset(powerup2, paddles)
 
-def powerup1_reset():
+def powerup1_reset(powerup1, paddles):
    
-    main["powerup1"]["powerup1_x"] = 390
-    main["powerup1"]["powerup1_y"] = 290
-    main["paddles"]["leftp_x"] = -100  
-    main["paddles"]["rightp_x"] = -100
+    powerup1["powerup1_x"] = 390
+    powerup1["powerup1_y"] = 290
+    paddles["leftp_x"] = -100  
+    paddles["rightp_x"] = -100
 
-def powerup2_reset():
+def powerup2_reset(powerup2, paddles):
    
     #paddle height dependent of powerup2
-    main["paddles"]["powerup2_left_paddle_height"] = 80
-    main["paddles"]["powerup2_right_paddle_height"] = 80
-    if main["paddles"]["powerup2_left_paddle_height"] == 160:
-        main["paddles"]["left_y"] += 40        
-        main["paddles"]["leftp_y"] += 40
-        main["paddles"]["powerup2_left_paddle_height"] = 80
-    elif main["paddles"]["powerup2_right_paddle_height"] == 160:
-        main["paddles"]["right_y"] += 40
-        main["paddles"]["rightp_y"] += 40
-        main["paddles"]["powerup2_right_paddle_height"] = 80
-    main["powerup2"]["powerup2_x"] = 390
-    main["powerup2"]["powerup2_y"] = 290
+    paddles["powerup2_left_paddle_height"] = 80
+    paddles["powerup2_right_paddle_height"] = 80
+    if paddles["powerup2_left_paddle_height"] == 160:
+        paddles["left_y"] += 40        
+        paddles["leftp_y"] += 40
+        paddles["powerup2_left_paddle_height"] = 80
+    elif paddles["powerup2_right_paddle_height"] == 160:
+        paddles["right_y"] += 40
+        paddles["rightp_y"] += 40
+        paddles["powerup2_right_paddle_height"] = 80
+    powerup2["powerup2_x"] = 390
+    powerup2["powerup2_y"] = 290
 
-def ball_reset():
+def ball_reset(ball):
     #ball position
-    main["ball"]["ball_x"] = 390
-    main["ball"]["ball_y"] = 290
+    ball["ball_x"] = 390
+    ball["ball_y"] = 290
    
-def continous_movement_paddles():
+def continous_movement_paddles(paddles):
     #moving paddles
     #move left paddles
-    if main["paddles"]["left_key"] == "w":
-        main["paddles"]["left_y"] += -2
-        main["paddles"]["leftp_y"] += 2
-    if main["paddles"]["left_key"] == "s":
-        main["paddles"]["left_y"] += 2  
-        main["paddles"]["leftp_y"] += -2
+    if paddles["left_key"] == "w":
+        paddles["left_y"] += -2
+        paddles["leftp_y"] += 2
+    if paddles["left_key"] == "s":
+        paddles["left_y"] += 2  
+        paddles["leftp_y"] += -2
     #move right paddles
-    if main["paddles"]["right_key"] == "i":
-        main["paddles"]["right_y"] += -2
-        main["paddles"]["rightp_y"] += 2
-    if main["paddles"]["right_key"] == "k":
-        main["paddles"]["right_y"] += 2  
-        main["paddles"]["rightp_y"] += -2
+    if paddles["right_key"] == "i":
+        paddles["right_y"] += -2
+        paddles["rightp_y"] += 2
+    if paddles["right_key"] == "k":
+        paddles["right_y"] += 2  
+        paddles["rightp_y"] += -2
        
-def auto_object_movements():
+def auto_object_movements(powerup1, powerup2, ball):
    
     #moving powerups
-    main["powerup1"]["powerup1_x"] += main["powerup1"]["powerup1_dx"]
-    main["powerup1"]["powerup1_y"] += main["powerup1"]["powerup1_dy"]
-    main["powerup2"]["powerup2_x"] += main["powerup2"]["powerup2_dx"]
-    main["powerup2"]["powerup2_y"] += main["powerup2"]["powerup2_dy"]
+    powerup1["powerup1_x"] += powerup1["powerup1_dx"]
+    powerup1["powerup1_y"] += powerup1["powerup1_dy"]
+    powerup2["powerup2_x"] += powerup2["powerup2_dx"]
+    powerup2["powerup2_y"] += powerup2["powerup2_dy"]
 
     #moving ball    
-    main["ball"]["ball_x"] = main["ball"]["ball_x"] + main["ball"]["ball_dx"]
-    main["ball"]["ball_y"] = main["ball"]["ball_y"] + main["ball"]["ball_dy"]
+    ball["ball_x"] = ball["ball_x"] + ball["ball_dx"]
+    ball["ball_y"] = ball["ball_y"] + ball["ball_dy"]
    
-def auto_object_area_boundaries():
+def auto_object_area_boundaries(ball, powerup1, powerup2):
    
         #bounce off bottom/top screen
-    if main["ball"]["ball_y"] < 0 or main["ball"]["ball_y"] > 580:
-        main["ball"]["ball_dy"] = main["ball"]["ball_dy"] * -1
+    if ball["ball_y"] < 0 or ball["ball_y"] > 580:
+        ball["ball_dy"] = ball["ball_dy"] * -1
        
         #bounce off bottom/top screen
-    if main["powerup1"]["powerup1_y"] >= 560 or main["powerup1"]["powerup1_y"] <= 0:
-        main["powerup1"]["powerup1_dy"] = main["powerup1"]["powerup1_dy"] * -1
-    if main["powerup2"]["powerup2_y"] >= 560 or main["powerup2"]["powerup2_y"] <= 0:
-        main["powerup2"]["powerup2_dy"] = main["powerup2"]["powerup2_dy"] * -1
+    if powerup1["powerup1_y"] >= 560 or powerup1["powerup1_y"] <= 0:
+        powerup1["powerup1_dy"] = powerup1["powerup1_dy"] * -1
+    if powerup2["powerup2_y"] >= 560 or powerup2["powerup2_y"] <= 0:
+        powerup2["powerup2_dy"] = powerup2["powerup2_dy"] * -1
        
         #bounce in yellow rectangle
-    if main["powerup1"]["powerup1_x"] >= 560 or main["powerup1"]["powerup1_x"] <= 200:
-        main["powerup1"]["powerup1_dx"] = main["powerup1"]["powerup1_dx"] * -1
-    if main["powerup2"]["powerup2_x"] >= 560 or main["powerup2"]["powerup2_x"] <= 200:
-        main["powerup2"]["powerup2_dx"] = main["powerup2"]["powerup2_dx"] * -1
+    if powerup1["powerup1_x"] >= 560 or powerup1["powerup1_x"] <= 200:
+        powerup1["powerup1_dx"] = powerup1["powerup1_dx"] * -1
+    if powerup2["powerup2_x"] >= 560 or powerup2["powerup2_x"] <= 200:
+        powerup2["powerup2_dx"] = powerup2["powerup2_dx"] * -1
 
-def score_counter():
+def score_counter(ball, other):
     #scoring
         #going off left or right edge of screen
         #check right edge/left point
-    if main["ball"]["ball_x"] > 780:
-        main["other"]["left_score"] += 1
-        ball_reset()
+    if ball["ball_x"] > 780:
+        other["left_score"] += 1
+        ball_reset(ball)
            
         #check left edge/right point
-    elif main["ball"]["ball_x"] < 0:
-        main["other"]["right_score"] += 1
-        ball_reset()
+    elif ball["ball_x"] < 0:
+        other["right_score"] += 1
+        ball_reset(ball)
        
-def paddle_ball_bounce():
+def paddle_ball_bounce(ball, paddles):
         #left paddle bounce    
-    if main["ball"]["ball_x"] <= 40 and main["ball"]["ball_y"] > main["paddles"]["left_y"] and main["ball"]["ball_y"] < (main["paddles"]["left_y"] + main["paddles"]["powerup2_left_paddle_height"]):
-        main["ball"]["ball_dx"] = main["ball"]["ball_dx"] * -1
+    if ball["ball_x"] <= 40 and ball["ball_y"] > paddles["left_y"] and ball["ball_y"] < (paddles["left_y"] + paddles["powerup2_left_paddle_height"]):
+        ball["ball_dx"] = ball["ball_dx"] * -1
         # left powerup1 paddle bounce
-    elif main["ball"]["ball_x"] <= 40 and main["ball"]["ball_y"] > main["paddles"]["leftp_y"] and main["ball"]["ball_y"] < (main["paddles"]["leftp_y"] + main["paddles"]["powerup2_left_paddle_height"]):
-        main["ball"]["ball_dx"] = main["ball"]["ball_dx"] * -1
+    elif ball["ball_x"] <= 40 and ball["ball_y"] > paddles["leftp_y"] and ball["ball_y"] < (paddles["leftp_y"] + paddles["powerup2_left_paddle_height"]):
+        ball["ball_dx"] = ball["ball_dx"] * -1
         #right paddle bounce
-    if main["ball"]["ball_x"] >= 750 and main["ball"]["ball_y"] > main["paddles"]["right_y"] and main["ball"]["ball_y"] < (main["paddles"]["right_y"] + main["paddles"]["powerup2_right_paddle_height"]):
-        main["ball"]["ball_dx"] = main["ball"]["ball_dx"] * -1
+    if ball["ball_x"] >= 750 and ball["ball_y"] > paddles["right_y"] and ball["ball_y"] < (paddles["right_y"] + paddles["powerup2_right_paddle_height"]):
+        ball["ball_dx"] = ball["ball_dx"] * -1
         #right powerup1 paddle bounce
-    elif main["ball"]["ball_x"] >= 750 and main["ball"]["ball_y"] > main["paddles"]["rightp_y"] and main["ball"]["ball_y"] < (main["paddles"]["rightp_y"] + main["paddles"]["powerup2_right_paddle_height"]):
-         main["ball"]["ball_dx"] = main["ball"]["ball_dx"] * -1
+    elif ball["ball_x"] >= 750 and ball["ball_y"] > paddles["rightp_y"] and ball["ball_y"] < (paddles["rightp_y"] + paddles["powerup2_right_paddle_height"]):
+         ball["ball_dx"] = ball["ball_dx"] * -1
          
-def powerup_ball_collisions():
+def powerup_ball_collisions(powerup1, ball, paddles, other, powerup2):
     #collision powerup1 and ball
-    if abs(main["powerup1"]["powerup1_x"] - main["ball"]["ball_x"]) <= 30 and abs(main["powerup1"]["powerup1_y"] - main["ball"]["ball_y"]) <= 30:
-        if main["ball"]["ball_dx"] > 0:
-            main["paddles"]["leftp_x"] = 30
-        elif main["ball"]["ball_dx"] < 0:
-            main["paddles"]["rightp_x"] = 770
-        main["other"]["counter1"] = 1
-        main["powerup1"]["powerup1_x"] = 1000
-        main["other"]["last_hit1"] = millis()
+    if abs(powerup1["powerup1_x"] - ball["ball_x"]) <= 30 and abs(powerup1["powerup1_y"] - ball["ball_y"]) <= 30:
+        if ball["ball_dx"] > 0:
+            paddles["leftp_x"] = 30
+        elif ball["ball_dx"] < 0:
+            paddles["rightp_x"] = 770
+        other["counter1"] = 1
+        powerup1["powerup1_x"] = 1000
+        other["last_hit1"] = millis()
        
     #collision powerup2
-    if abs(main["powerup2"]["powerup2_x"] - main["ball"]["ball_x"]) <= 30 and abs(main["powerup2"]["powerup2_y"] - main["ball"]["ball_y"]) <= 30:
-        if main["ball"]["ball_dx"] > 0:
-            main["paddles"]["powerup2_left_paddle_height"] = 160
-            main["paddles"]["left_y"] -= 40
-            main["paddles"]["leftp_y"] -= 40
-        elif main["ball"]["ball_dx"] < 0:
-            main["paddles"]["powerup2_right_paddle_height"] = 160
-            main["paddles"]["right_y"] -= 40
-            main["paddles"]["rightp_y"] -= 40
-        main["other"]["counter2"] = 1
-        main["powerup2"]["powerup2_x"] = 1000
-        main["other"]["last_hit2"] = millis()
+    if abs(powerup2["powerup2_x"] - ball["ball_x"]) <= 30 and abs(powerup2["powerup2_y"] - ball["ball_y"]) <= 30:
+        if ball["ball_dx"] > 0:
+            paddles["powerup2_left_paddle_height"] = 160
+            paddles["left_y"] -= 40
+            paddles["leftp_y"] -= 40
+        elif ball["ball_dx"] < 0:
+            paddles["powerup2_right_paddle_height"] = 160
+            paddles["right_y"] -= 40
+            paddles["rightp_y"] -= 40
+        other["counter2"] = 1
+        powerup2["powerup2_x"] = 1000
+        other["last_hit2"] = millis()
 
        
-def draw_screen():
+def draw_screen(other, powerup1, powerup2, paddles, ball):
     text("PONG", 350,40)
-    text(main["other"]["left_score"], 40, 40)
-    text(main["other"]["right_score"], 760,40)
+    text(other["left_score"], 40, 40)
+    text(other["right_score"], 760,40)
    
     #powerup area
     fill(255,215,0)
@@ -152,23 +152,23 @@ def draw_screen():
    
         #Powerups
     fill(255, 0,  0)
-    rect(main["powerup1"]["powerup1_x"], main["powerup1"]["powerup1_y"],40,40)
+    rect(powerup1["powerup1_x"], powerup1["powerup1_y"],40,40)
     fill(0, 0, 255)
-    rect(main["powerup2"]["powerup2_x"], main["powerup2"]["powerup2_y"],40,40)
+    rect(powerup2["powerup2_x"], powerup2["powerup2_y"],40,40)
        
         #paddles
     fill(255, 255, 255)
-    rect(30,main["paddles"]["left_y"],10,main["paddles"]["powerup2_left_paddle_height"]) #left paddle
+    rect(30,paddles["left_y"],10,paddles["powerup2_left_paddle_height"]) #left paddle
     fill(255, 255, 255)
-    rect(770,main["paddles"]["right_y"],10,main["paddles"]["powerup2_right_paddle_height"]) #right paddle
+    rect(770,paddles["right_y"],10,paddles["powerup2_right_paddle_height"]) #right paddle
    
     #powerup1_paddle
     fill(0, 128, 0)
-    rect(main["paddles"]["leftp_x"],main["paddles"]["leftp_y"],10,main["paddles"]["powerup2_left_paddle_height"]) #left paddle
+    rect(paddles["leftp_x"],paddles["leftp_y"],10,paddles["powerup2_left_paddle_height"]) #left paddle
     fill(0, 128, 0)
-    rect(main["paddles"]["rightp_x"],main["paddles"]["rightp_y"],10,main["paddles"]["powerup2_right_paddle_height"]) #right paddle
+    rect(paddles["rightp_x"],paddles["rightp_y"],10,paddles["powerup2_right_paddle_height"]) #right paddle
    
 
     #ball
     fill(255, 255, 255)
-    rect(main["ball"]["ball_x"],main["ball"]["ball_y"],20,20)
+    rect(ball["ball_x"],ball["ball_y"],20,20)
